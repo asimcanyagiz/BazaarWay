d83e000f028b87a14ec5d6d89ecee577a4962ced
Asim Can Yagiz <meycasim@gmail.com>
2022-10-26 13:21:16 +0300

    Firebase Auth and Firestore completed. Test results are succesfull

--- BazaarWay/Screens/Auth/Controller/AuthViewController.swift
+++ BazaarWay/Screens/Auth/Controller/AuthViewController.swift
@@ -6,11 +6,44 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 final class AuthViewController: UIViewController {
     
     // MARK: - View Model
     private let viewModel: AuthViewModel
+    
+    //Cases for auth screen calls
+    enum AuthType: String {
+        case signIn = "Sign In"
+        case signUp = "Sign Up"
+        
+        init(text: String) {
+            switch text {
+            case "Sign In":
+                self = .signIn
+            case "Sign Up":
+                self = .signUp
+            default:
+                self = .signIn
+            }
+        }
+    }
+    
+    var authType: AuthType = .signIn {
+        didSet {
+            titleLabel.text = title
+            submitButton.setTitle(title, for: .normal)
+        }
+    }
+    
+    
+    //MARK: - UI ELEMENTS
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
     
     // MARK: - Init
     init(viewModel: AuthViewModel) {
@@ -29,6 +62,55 @@ final class AuthViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //Control the inputs for cases and catch the results
+        viewModel.changeHandler = { change in
+            switch change {
+            case .didErrorOccurred(let error):
+                print(error)
+            case .didSignUpSuccessful:
+                print("SIGN UP SUCCESSFUL!")
+            }
+        }
+        
+        title = "Auth"
     }
+    
+    
+    
+    @IBAction func didSegmentedButtonPressed(_ sender: UISegmentedControl) {
+        
+        let title = segmentedControl.titleForSegment(at: segmentedControl.selectedSegmentIndex)
+        authType = AuthType(text: title ?? "Sign In")
+    }
+    
+    
+    
+    
+    @IBAction func didSubmitButtonPressed(_ sender: UIButton) {
+        
+        //inputs
+        guard let credential = emailTextField.text,
+              let password = passwordTextField.text else {
+            return
+        }
+        switch authType {
+        case .signIn:
+            viewModel.signIn(email: credential,
+                             password: password,
+                             completion: { [weak self] in
+                guard let self = self else { return }
+                print("succesfully login")
+                
+            })
+        case .signUp:
+            viewModel.signUp(email: credential,
+                             password: password)
+        }
+        
+    }
+    
+    
+    
+    
 
 }
--- BazaarWay/Screens/Auth/ViewModel/AuthViewModel.swift
+++ BazaarWay/Screens/Auth/ViewModel/AuthViewModel.swift
@@ -6,7 +6,73 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+
+//Cases for connect with firebase
+enum AuthViewModelChange {
+    case didErrorOccurred(_ error: Error)
+    case didSignUpSuccessful
+}
 
 final class AuthViewModel {
     
+    private let db = Firestore.firestore()
+    private let defaults = UserDefaults.standard
+    
+    var changeHandler: ((AuthViewModelChange) -> Void)?
+    
+    //Function for signup
+    func signUp(email: String, password: String) {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                self.changeHandler?(.didErrorOccurred(error))
+                return
+            }
+            
+            //We create a User with User Model
+            let user = User(username: authResult?.user.displayName,
+                            email: authResult?.user.email,
+                            basket: [])
+            do {
+                guard let data = try user.dictionary,
+                      let id = authResult?.user.uid else {
+                    return
+                }
+                
+                self.defaults.set(id, forKey: "uid")
+                
+                self.db.collection("users").document(id).setData(data) { error in
+                    
+                    if let error = error {
+                        self.changeHandler?(.didErrorOccurred(error))
+                    } else {
+                        self.changeHandler?(.didSignUpSuccessful)
+                    }
+                }
+            } catch {
+                self.changeHandler?(.didErrorOccurred(error))
+            }
+        }
+    }
+    
+    //Sign up function
+    func signIn(email: String,
+                password: String,
+                completion: @escaping () -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                self.changeHandler?(.didErrorOccurred(error))
+                return
+            }
+            
+            guard let id = authResult?.user.uid else {
+                return
+            }
+            
+            self.defaults.set(id, forKey: "uid")
+            
+            completion()
+        }
+    }
 }
