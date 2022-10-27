0a39c319528411fa91a96170bdc1a7c0f7622f53
Asim Can Yagiz <meycasim@gmail.com>
2022-10-27 22:19:39 +0300

    AlertPresentable created for call alerts more harmonious

---
+++ BazaarWay/App/Core/Helpers/AlertPresentable.swift
@@ -0,0 +1,40 @@
+//
+//  AlertPresentable.swift
+//  BazaarWay
+//
+//  Created by Asım can Yağız on 27.10.2022.
+//
+
+import UIKit
+
+protocol AlertPresentable { }
+
+//With this protocol we can manage speacial alerts
+extension AlertPresentable where Self: UIViewController {
+    func showAlert(title: String? = nil,
+                   message: String? = nil,
+                   cancelButtonTitle: String? = nil,
+                   handler: ((UIAlertAction) -> Void)? = nil ) {
+        let alertController = UIAlertController(title: title,
+                                                message: message,
+                                                preferredStyle: .alert)
+        
+        let defaultAction = UIAlertAction(title: "OK",
+                                          style: .default,
+                                          handler: handler)
+        
+        if let cancelButtonTitle = cancelButtonTitle {
+            let cancelAction = UIAlertAction(title: cancelButtonTitle,
+                                             style: .cancel)
+            alertController.addAction(cancelAction)
+        }
+        
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true)
+    }
+    
+    func showError(_ error: Error) {
+        showAlert(title: "Error Occurred",
+                  message: error.localizedDescription)
+    }
+}
